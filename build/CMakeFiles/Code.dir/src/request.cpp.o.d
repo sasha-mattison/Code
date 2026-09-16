a7409b8e0d3da41ee9d5b8324b76a33149826696
Sasha Mattison <sashastoolbox@gmail.com>
2026-09-16 08:13:09 -0700

    Built a Request class that takes in params and return a json ready to send to the model

--- build/CMakeFiles/Code.dir/src/request.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Code.dir/src/request.cpp.o: \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/SDKSettings.json \
-  /Users/sasha/Documents/GitHub/Code/src/request.cpp \
-  /Users/sasha/Documents/GitHub/Code/include/request.h
